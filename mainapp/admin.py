763ff42760186da6ad5fe328925c002de8e3aa6d
Solaer <endercyrte@gmail.com>
2022-07-27 00:25:24 +0300

    Lesson 4. Data from files

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Contact, Product, ProductCategory
+
+admin.site.register(ProductCategory)
+admin.site.register(Product)
+admin.site.register(Contact)
+
 # Register your models here.
